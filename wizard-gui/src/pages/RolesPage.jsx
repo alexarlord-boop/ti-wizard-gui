4afd5b823564d892b74c4ff9b5cd502741e5dffc
alexarlord-boop <rezerw0.1@yandex.ru>
2024-09-13 23:11:30 +0300

    TI-WIZARD Updated role cards + badge

--- wizard-gui/src/pages/RolesPage.jsx
+++ wizard-gui/src/pages/RolesPage.jsx
@@ -1,38 +1,29 @@
+import React from "react";
+import { useQuery } from "react-query";
 import RoleCard from "../components/custom/RoleCard.jsx";
 import Breadcrumbs from "../components/custom/Breadcrumbs.jsx";
-import {useEffect, useState} from "react";
-import {getRolesData} from "../services/roleService.js";
-import {Spinner} from "../components/ui/Loader.jsx";
-
+import { getRolesData } from "../services/roleService.js";
+import { Spinner } from "../components/ui/Loader.jsx";
 
 function RolesPage() {
+    const { data: roles, isLoading, isError } = useQuery('roles', getRolesData);
 
-    const [roles, setRoles] = useState([]);
+    if (isLoading) {
+        return <Spinner size="small" className="mt-20" />;
+    }
 
-    useEffect(() => {
-        const fetchRoles = async () => {
-            const roles = await getRolesData();
-            setRoles(roles);
-        };
-
-        fetchRoles();
-    }, []);
-
+    if (isError) {
+        return <div>Error fetching roles</div>;
+    }
 
     return (
         <>
-            <Breadcrumbs itemList={[{path: '/', label: 'Home'}, { path: '/roles', label: 'My Roles' }]} />
-            {roles.length > 0 ? (
-                <>
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                        {roles.map((role, index) => (
-                            <RoleCard key={index} data={role.data} />
-                        ))}
-                    </div>
-                </>
-            ) : (
-                <Spinner size="small" className="mt-20" />
-            )}
+            <Breadcrumbs itemList={[{ path: '/', label: 'Home' }, { path: '/roles', label: 'My Roles' }]} />
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                {roles.map((role, index) => (
+                    <RoleCard key={index} data={role.data} entityType={role.type}/>
+                ))}
+            </div>
         </>
     );
 }
