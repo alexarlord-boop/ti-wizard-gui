8964ea67cd166a8258a0f5f8c6ab996ad58e5c85
alexarlord-boop <rezerw0.1@yandex.ru>
2024-10-08 20:21:55 +0300

    TI-WIZARD Added incubator logo

--- wizard-gui/src/components/MainLayout.jsx
+++ wizard-gui/src/components/MainLayout.jsx
@@ -1,5 +1,6 @@
 import NavBar from "./custom/NavBar.jsx";
 import {TourProvider} from "./context/TourContext.jsx";
+import {SiteFooter} from "./custom/Footer.jsx";
 
 
 export default function MainLayout({children}) {
@@ -9,6 +10,7 @@ export default function MainLayout({children}) {
             <main className=" container mx-auto p-4">
                 {children}
             </main>
+           <SiteFooter/>
         </TourProvider>
 
     )
