24adcadd52deb4919ea44c8673e2f819fff697c1
alexarlord-boop <rezerw0.1@yandex.ru>
2024-10-29 17:40:26 +0200

    TI-WIZARD Updated coloring

--- wizard-gui/src/pages/RemotePage/RemoteEntitiesDataTable/RegistrationFilter.jsx
+++ wizard-gui/src/pages/RemotePage/RemoteEntitiesDataTable/RegistrationFilter.jsx
@@ -10,7 +10,7 @@ const RegistrationFilter = ({ selectedRegistrations, handleRegistrationChange, i
                     <span className="sr-only">Open menu</span>
                     Registered in
                     {selectedRegistrations.length > 0 && (
-                        <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-0 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full">
+                        <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-0 text-xs font-bold leading-none text-green-100 bg-green-500 rounded-full">
                             {selectedRegistrations.length}
                         </span>
                     )}
--- wizard-gui/src/pages/RemotePage/RemoteEntitiesDataTable/RoleFilter.jsx
+++ wizard-gui/src/pages/RemotePage/RemoteEntitiesDataTable/RoleFilter.jsx
@@ -13,7 +13,7 @@ const RoleFilter = ({selectedRoles, handleRoleChange, isRoleDropdownOpen, setIsR
             <Button variant="outline" className="relative">
                 Role
                 {selectedRoles.length > 0 && (
-                    <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-0 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full">
+                    <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-0 text-xs font-bold leading-none text-green-100 bg-green-500 rounded-full">
                             {selectedRoles.length}
                         </span>
                 )}
