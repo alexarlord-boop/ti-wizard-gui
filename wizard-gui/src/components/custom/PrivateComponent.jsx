73342b983b7e3ea50c35f97f7c8cbf4341e936d4
alexarlord-boop <rezerw0.1@yandex.ru>
2024-10-31 14:12:49 +0200

    TI-WIZARD Added private component in the profile dropdown

---
+++ wizard-gui/src/components/custom/PrivateComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+// Function to simulate authentication check
+const isAuthenticated = () => {
+    // Implement actual authentication logic here (e.g., check token in local storage, session, etc.)
+    return !!localStorage.getItem('access_token'); // Assume auth token is stored in localStorage
+};
+
+export default function PrivateComponent({ children, fallback=null }) {
+    return isAuthenticated() ? children : fallback;
+}
--- wizard-gui/src/components/custom/ProfileDropdown.jsx
+++ wizard-gui/src/components/custom/ProfileDropdown.jsx
@@ -8,8 +8,10 @@ import {
 } from "@/components/ui/dropdown-menu";
 import {useTour} from '../context/TourContext';
 import {useTranslation} from 'react-i18next';
-import {ExitIcon, GearIcon, PersonIcon} from "@radix-ui/react-icons";
-import {useState} from "react";
+import {EnterIcon, ExitIcon, GearIcon, PersonIcon} from "@radix-ui/react-icons";
+import {toast} from "sonner";
+import PrivateComponent from "./PrivateComponent.jsx";
+import PrivateRoute from "./PrivteRoute.jsx";
 
 export default function ProfileDropdown({children}) {
     const {t} = useTranslation();
@@ -20,61 +22,57 @@ export default function ProfileDropdown({children}) {
             startTour();  // Trigger the tour for the current page
         } else {
             console.log("No tour is set for this page");
+            toast.error('No tour is set for this page');
         }
     };
 
-    // return (
-    //     <DropdownMenu>
-    //         <DropdownMenuTrigger className="focus:outline-none">{children}</DropdownMenuTrigger>
-    //         <DropdownMenuContent>
-    //             <DropdownMenuSeparator />
-    //             <DropdownMenuItem className="bg-green-100" onClick={handleTour}>{t('navbar.startTour')}</DropdownMenuItem>
-    //             <DropdownMenuItem>{t('navbar.logs')}</DropdownMenuItem>
-    //             <DropdownMenuItem>{t('navbar.cronTab')}</DropdownMenuItem>
-    //             <DropdownMenuItem>{t('navbar.contactDetails')}</DropdownMenuItem>
-    //         </DropdownMenuContent>
-    //     </DropdownMenu>
-    // );
-
-    const [isOpen, setIsOpen] = useState(false);
+    const handleLogin = () => {
+        window.location.href = '/login';
+    }
 
     const handleLogout = () => {
-        // Implement your logout logic here
         localStorage.removeItem('access_token');
         localStorage.removeItem('refresh_token');
-        // Optionally redirect the user to the login page
         window.location.href = '/login';
     };
     return (
         <DropdownMenu>
             <DropdownMenuTrigger className="focus:outline-none flex items-center">
-                <PersonIcon className="h-5 w-5 mr-2"/>
-                {/*{children}*/}
-                { localStorage.getItem('username') }
+                <PersonIcon className="h-5 w-5 mr-2 bg-gray-100 rounded-full"/>
+                {localStorage.getItem('access_token') !== null && localStorage.getItem('username')}
 
 
             </DropdownMenuTrigger>
 
             <DropdownMenuContent className="mt-2 w-48 bg-white rounded-md shadow-lg">
-                <DropdownMenuItem className="hover:bg-gray-100">
-                    <a href="/profile" className="flex items-center w-full text-gray-700">
-                        <GearIcon className="mr-2"/>
-                        Settings
-                    </a>
-                </DropdownMenuItem>
+                <PrivateComponent>
+                    <DropdownMenuItem className="hover:bg-gray-100">
+                        <a href="/profile" className="flex items-center w-full text-gray-700">
+                            <GearIcon className="mr-2"/>
+                            Settings
+                        </a>
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator/>
+                </PrivateComponent>
 
-                <DropdownMenuSeparator/>
+
                 <DropdownMenuItem onClick={handleTour}>{t('navbar.startTour')}</DropdownMenuItem>
-                <DropdownMenuItem>{t('navbar.logs')}</DropdownMenuItem>
-                <DropdownMenuItem>{t('navbar.cronTab')}</DropdownMenuItem>
-
+                <PrivateComponent> <DropdownMenuItem>{t('navbar.logs')}</DropdownMenuItem></PrivateComponent>
+                <PrivateComponent><DropdownMenuItem>{t('navbar.cronTab')}</DropdownMenuItem></PrivateComponent>
 
                 <DropdownMenuSeparator/>
 
-                <DropdownMenuItem className="hover:bg-red-100 text-red-700" onClick={handleLogout}>
-                    <ExitIcon className="mr-2"/>
-                    Logout
-                </DropdownMenuItem>
+                <PrivateComponent fallback={
+                    <DropdownMenuItem className="hover:bg-red-100 text-green-700" onClick={handleLogin}>
+                        <EnterIcon className="mr-2"/>
+                        Login
+                    </DropdownMenuItem>
+                }>
+                    <DropdownMenuItem className="hover:bg-red-100 text-red-700" onClick={handleLogout}>
+                        <ExitIcon className="mr-2"/>
+                        Logout
+                    </DropdownMenuItem>
+                </PrivateComponent>
             </DropdownMenuContent>
         </DropdownMenu>
     );
