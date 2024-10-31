d709873219ce1b746ed009e995839fedaf796691
alexarlord-boop <rezerw0.1@yandex.ru>
2024-10-31 15:31:19 +0200

    TI-WIZARD Updated RA title

--- wizard-gui/src/pages/RemotePage/RemoteEntitiesDataTable/RegistrationFilter.jsx
+++ wizard-gui/src/pages/RemotePage/RemoteEntitiesDataTable/RegistrationFilter.jsx
@@ -8,7 +8,7 @@ const RegistrationFilter = ({ selectedRegistrations, handleRegistrationChange, i
             <DropdownMenuTrigger asChild>
                 <Button variant="outline" className="relative">
                     <span className="sr-only">Open menu</span>
-                    Registered in
+                    Published in
                     {selectedRegistrations.length > 0 && (
                         <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-0 text-xs font-bold leading-none text-green-100 bg-green-500 rounded-full">
                             {selectedRegistrations.length}
